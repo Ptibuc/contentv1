9143c3b4e1db63e7ff005fa6c73c213155ae06f4
ptibuc <ptibuc@gmail.com>
2016-11-27 16:32:36 +0100

    Amélioration de la gestion des sites

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,21 +1,30 @@
 class DashboardController < ApplicationController
   def index
+    if !session[:site] && current_user.sites.count==1
+      site = Site.find_by user_id: current_user.id
+      assign_site_to_session(site.id)
+    end
   end
 
   def select_site
     # passer en session la valeur du site sélectionné
-    if Site.exists?(params[:site]) && Site.find(params[:site]).user_id==current_user.id
-      session[:site] = params[:site]
-      redirect_to dashboard_path, notice: "Site sélectionné"
-    else
-      redirect_to dashboard_path, alert: "Erreur, impossible de sélectionner ce site. Vérifiez qu'il est bien à vous ou qu'il existe."
-    end
-
+    assign_site_to_session(params[:site])
   end
 
   def change_site
     session[:site] = nil
     redirect_to dashboard_path
+  end
+
+  private
+
+  def assign_site_to_session(site)
+    if Site.exists?(site) && Site.find(site).user_id==current_user.id
+      session[:site] = site
+      redirect_to dashboard_path, notice: "Site sélectionné"
+    else
+      redirect_to dashboard_path, alert: "Erreur, impossible de sélectionner ce site. Vérifiez qu'il est bien à vous ou qu'il existe."
+    end
   end
 
 end
